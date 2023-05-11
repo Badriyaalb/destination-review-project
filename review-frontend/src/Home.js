8cb603fefd5c06ab877439a634b35aefabb68908
Badriyaalb <badriya-albloushi@outlook.com>
2023-05-11 11:38:32 +0530

    good

--- review-frontend/src/Home.js
+++ review-frontend/src/Home.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Attractionlist from "./components/Attractionlist";
-import IMGW from "./imgs/place-img.jpg";
+import IMGWorld from "./imgs/place-img.jpg";
 import motiongate from "./imgs/place-motiongate.webp";
 import readyset from "./imgs/place-ready.png";
 import kidzania from "./imgs/place-kidzania.jpg";
@@ -13,11 +13,11 @@ function Home(){
         <>
         <Header/>
         <div>
-            <Attractionlist placeimg={IMGW}  location="Dubai" ticket="345-350" day="Everyday" rating="No rating" /> 
-            <Attractionlist placeimg={motiongate}  location="Dubai" ticket="240-330" day="Everyday" rating="No rating" />  
-            <Attractionlist placeimg={readyset}  location="Dubai" ticket="85-150" day="Everyday" rating="No rating" />  
-            <Attractionlist placeimg={kidzania}  location="Dubai-AbuDhabi" ticket="195-260" day="Everyday" rating="No rating" />  
-            <Attractionlist placeimg={gardenglow}  location="Dubai" ticket="60-89" day="Everyday" rating="No rating" />    
+            <Attractionlist placeimg={IMGWorld} placename="IMG World" location="Dubai" ticket="345-350" day="Everyday" rating="No rating" /> 
+            <Attractionlist placeimg={motiongate} placename="Motiongate" location="Dubai" ticket="240-330" day="Everyday" rating="No rating" />  
+            <Attractionlist placeimg={readyset} placename="Ready Set Go" location="Dubai" ticket="85-150" day="Everyday" rating="No rating" />  
+            <Attractionlist placeimg={kidzania} placename="Kidzania" location="Dubai-AbuDhabi" ticket="195-260" day="Everyday" rating="No rating" />  
+            <Attractionlist placeimg={gardenglow} placename="Garden Glow" location="Dubai" ticket="60-89" day="Everyday" rating="No rating" />    
         </div>
         <Footer/>
         </>
