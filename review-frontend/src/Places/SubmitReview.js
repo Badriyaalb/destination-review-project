8b4f6ebbddaec68dbbd953cbe565577cb5473828
Badriyaalb <badriya-albloushi@outlook.com>
2023-05-17 12:01:46 +0530

    hi

--- review-frontend/src/Places/SubmitReview.js
+++ review-frontend/src/Places/SubmitReview.js
@@ -8,15 +8,19 @@ function SubmitReview(){
 return(
 
     <>
-    <Header/>
-    <h1> Add You Review</h1>
-    <div id="flexnameandthumbs">
-    <textarea id="UserNamett" placeholder="Name"></textarea> <Thumbs/> <br/>
-    </div>
-    <textarea id="UserReviewtt" placeholder="Review"></textarea><br/>
-    <button onclick="" id="Submitbuttonttt"> Submit</button>
+    <Header/><br/>
+    <section id="reviewbox">
+     
+      <h1> Add You Review</h1>
+      <div id="flexnameandthumbs">
+        <textarea id="UserNamett" placeholder="Name"></textarea> <Thumbs/> <br/>
+      </div>
+      <textarea id="UserReviewtt" placeholder="Review"></textarea><br/>
+      <button onclick="" id="Submitbuttonttt"> Submit</button>
+     </section>
     <Footer/>
     </>
+    
 )
 
 }
